3c1d9884f03c8c1b779d46ee265e520bf51d2a6a
kimiyash <kimiyash@gmail.com>
2014-12-27 10:40:07 +0900

    Initial Commit

---
+++ SwiftChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftChat
+//
+//  Created by kimiyash on 2014/12/27.
+//  Copyright (c) 2014年 kimiyash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
